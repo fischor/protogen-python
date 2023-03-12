035691fa8a933cd2c80489ff4c93cd2642b29a9d
fischor <21199007+fischor@users.noreply.github.com>
2023-03-12 12:32:18 +0100

    test: switch to pytest

--- test/test_protogen.py
+++ test/test_protogen.py
@@ -5,97 +5,86 @@ import os
 from typing import Tuple, List
 
 
-class TestProtogen(unittest.TestCase):
+def test_protogen():
+    # test won't work if using relative path. protoc seems to have troubles
+    # finding files then:
+    #
+    #   - protoc-gen-empty=./test/plugin/main.py: warning: directory does not exist.
+    #   - vendor/google/api/annotations.proto: File does not reside within any path
+    #     specified using --proto_path (or -I).  You must specify a --proto_path which
+    #     encompasses this file.  Note that the proto_path must be an exact prefix of
+    #     the .proto file names -- protoc is too dumb to figure out when two paths
+    #     (e.g. absolute and relative) are equivalent (it's harder than you think).
+    #
+    pwd = os.getcwd()
+    vendor = os.path.join(pwd, "test", "vendor")
+    proto = os.path.join(pwd, "test", "vendor", "google", "api", "annotations.proto")
+    plugin = os.path.join(pwd, "test", "plugin", "main.py")
 
-    # Just e2e tests for now.
-    # One could create and store some protodescriptorsets using protoc.
-    # Create a CodeGeneratorRequest out of it and run the plugin using that
-    # one use Options(stdin=fake_stdin, stdout=fake_stdout) to fake stdin
-    # and out.
-    def test_e2e(self):
-        # test won't work if using relative path. protoc seems to have troubles
-        # finding files then:
-        #
-        #   - protoc-gen-empty=./test/plugin/main.py: warning: directory does not exist.
-        #   - vendor/google/api/annotations.proto: File does not reside within any path
-        #     specified using --proto_path (or -I).  You must specify a --proto_path which
-        #     encompasses this file.  Note that the proto_path must be an exact prefix of
-        #     the .proto file names -- protoc is too dumb to figure out when two paths
-        #     (e.g. absolute and relative) are equivalent (it's harder than you think).
-        #
-        pwd = os.getcwd()
-        vendor = os.path.join(pwd, "test", "vendor")
-        proto = os.path.join(
-            pwd, "test", "vendor", "google", "api", "annotations.proto"
-        )
-        plugin = os.path.join(pwd, "test", "plugin", "main.py")
+    # Create the output directory if it does not exists. protoc requires it
+    # to exist before generating output to it.
+    out_dir = os.path.join(pwd, "testout", "main")
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-        # Create the output directory if it does not exists. protoc requires it
-        # to exist before generating output to it.
-        out_dir = os.path.join(pwd, "testout", "main")
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
+    # Run protoc.
+    code, output = _run_protoc(
+        [
+            "protoc",
+            "-I",
+            vendor,
+            f"--plugin=protoc-gen-empty={plugin}",
+            f"--empty_out={out_dir}",
+            proto,
+        ]
+    )
+    assert output == ""
+    assert code == 0
 
-        # Run protoc.
-        code, output = _run_protoc(
-            [
-                "protoc",
-                "-I",
-                vendor,
-                f"--plugin=protoc-gen-empty={plugin}",
-                f"--empty_out={out_dir}",
-                proto,
-            ]
-        )
-        self.assertEqual(output, "")
-        self.assertEqual(code, 0)
+    # Check content of directory.
+    with open("test/golden/google/api/annotations.out") as f:
+        golden = f.read()
+    with open("testout/main/google/api/annotations.out") as f:
+        generated = f.read()
 
-        # Check content of directory.
-        with open("test/golden/google/api/annotations.out") as f:
-            golden = f.read()
-        with open("testout/main/google/api/annotations.out") as f:
-            generated = f.read()
+    assert golden == generated
 
-        self.assertEqual(golden, generated)
 
-    def test_parameter(self):
-        pwd = os.getcwd()
-        vendor = os.path.join(pwd, "test", "vendor")
-        proto = os.path.join(
-            pwd, "test", "vendor", "google", "api", "annotations.proto"
-        )
-        plugin = os.path.join(pwd, "test", "plugin", "parameter.py")
+def test_parameter():
+    pwd = os.getcwd()
+    vendor = os.path.join(pwd, "test", "vendor")
+    proto = os.path.join(pwd, "test", "vendor", "google", "api", "annotations.proto")
+    plugin = os.path.join(pwd, "test", "plugin", "parameter.py")
 
-        # Create the output directory if it does not exists. protoc requires it
-        # to exist before generating output to it.
-        out_dir = os.path.join(pwd, "testout", "params")
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-        self.maxDiff = None
+    # Create the output directory if it does not exists. protoc requires it
+    # to exist before generating output to it.
+    out_dir = os.path.join(pwd, "testout", "params")
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-        code, output = _run_protoc(
-            [
-                "protoc",
-                "-I",
-                vendor,
-                f"--plugin=protoc-gen-params={plugin}",
-                f"--params_opt=k1=v1,k2=v2;k3=v3,",
-                f"--params_opt=k4",
-                f"--params_opt=k5=v5",
-                f"--params_out=abc=x,5=2:{out_dir}",
-                proto,
-            ]
-        )
-        self.assertEqual(output, "")
-        self.assertEqual(code, 0)
+    code, output = _run_protoc(
+        [
+            "protoc",
+            "-I",
+            vendor,
+            f"--plugin=protoc-gen-params={plugin}",
+            f"--params_opt=k1=v1,k2=v2;k3=v3,",
+            f"--params_opt=k4",
+            f"--params_opt=k5=v5",
+            f"--params_out=abc=x,5=2:{out_dir}",
+            proto,
+        ]
+    )
+    assert output == ""
+    assert code == 0
 
-        # Check content of directory.
-        with open("test/golden/params.txt") as f:
-            golden = f.read()
-        with open("testout/params/out.txt") as f:
-            generated = f.read()
+    # Check content of directory.
+    with open("test/golden/params.txt") as f:
+        golden = f.read()
+    with open("testout/params/out.txt") as f:
+        generated = f.read()
 
-        self.assertEqual(golden, generated)
+    assert golden == generated
 
 
 def _run_protoc(args: List[str]) -> Tuple[int, str]:
--- test/test_registry.py
+++ test/test_registry.py
@@ -1,80 +1,97 @@
-import unittest
-
 import protogen
 import google.protobuf.descriptor_pb2
 
 
-class TestRegistry(unittest.TestCase):
-    def test_resolve_enum_type_name(self):
-        pass
+def test_registry_resolve_message_type():
+    registry = protogen.Registry()
 
-    def test_resolve_message_type_name(self):
-        registry = protogen.Registry()
+    acme_hello_file = protogen.File(
+        google.protobuf.descriptor_pb2.FileDescriptorProto(
+            name="acme/hello.proto", package="acme"
+        ),
+        False,
+        protogen.default_py_import_func,
+    )
 
-        acme_hello_file = protogen.File(
-            google.protobuf.descriptor_pb2.FileDescriptorProto(
-                name="acme/hello.proto", package="acme"
-            ),
-            False,
-            protogen.default_py_import_func,
-        )
-        acme_hello_message = protogen.Message(
-            google.protobuf.descriptor_pb2.DescriptorProto(name="Hello"),
-            acme_hello_file,
-            None,
-            None,
-        )
-        acme_hello_world_message = protogen.Message(
-            google.protobuf.descriptor_pb2.DescriptorProto(name="World"),
-            acme_hello_file,
-            acme_hello_message,
-            None,
-        )
+    acme_hello_message = protogen.Message(
+        google.protobuf.descriptor_pb2.DescriptorProto(name="Hello"),
+        acme_hello_file,
+        None,
+        None,
+    )
 
-        acme_cloud_library_v1_library_file = protogen.File(
-            google.protobuf.descriptor_pb2.FileDescriptorProto(
-                name="acme/cloud/library/v1/library.proto",
-                package="acme.cloud.library.v1",
-            ),
-            False,
-            protogen.default_py_import_func,
-        )
-        acme_cloud_library_v1_hello_message = protogen.Message(
-            google.protobuf.descriptor_pb2.DescriptorProto(name="Hello"),
-            acme_cloud_library_v1_library_file,
-            None,
-            None,
-        )
-        acme_cloud_library_v1_hello_world_message = protogen.Message(
-            google.protobuf.descriptor_pb2.DescriptorProto(name="World"),
-            acme_cloud_library_v1_library_file,
-            acme_cloud_library_v1_hello_message,
-            None,
-        )
+    acme_hello_world_message = protogen.Message(
+        google.protobuf.descriptor_pb2.DescriptorProto(name="World"),
+        acme_hello_file,
+        acme_hello_message,
+        None,
+    )
 
-        registry._register_message(acme_hello_message)
-        registry._register_message(acme_hello_world_message)
-        registry._register_message(acme_cloud_library_v1_hello_message)
-        registry._register_message(acme_cloud_library_v1_hello_world_message)
+    acme_cloud_library_v1_library_file = protogen.File(
+        google.protobuf.descriptor_pb2.FileDescriptorProto(
+            name="acme/cloud/library/v1/library.proto", package="acme.cloud.library.v1"
+        ),
+        False,
+        protogen.default_py_import_func,
+    )
 
-        got = registry.resolve_message("acme.cloud.library.v1.Hello", "World")
-        self.assertIsNotNone(got)
-        self.assertEqual(got.full_name, "acme.cloud.library.v1.Hello.World")
+    acme_cloud_library_v1_hello_message = protogen.Message(
+        google.protobuf.descriptor_pb2.DescriptorProto(name="Hello"),
+        acme_cloud_library_v1_library_file,
+        None,
+        None,
+    )
 
-        got = registry.resolve_message("acme.Hello", "World")
-        self.assertIsNotNone(got)
-        self.assertEqual(got.full_name, "acme.Hello.World")
+    acme_cloud_library_v1_hello_world_message = protogen.Message(
+        google.protobuf.descriptor_pb2.DescriptorProto(name="World"),
+        acme_cloud_library_v1_library_file,
+        acme_cloud_library_v1_hello_message,
+        None,
+    )
 
-        got = registry.resolve_message_type("acme.Hello", "World")
-        self.assertIsNotNone(got)
-        self.assertEqual(got.full_name, "acme.Hello.World")
+    google_protobuf_empty_file = protogen.File(
+        google.protobuf.descriptor_pb2.FileDescriptorProto(
+            name="google/protobuf/empty.proto", package="google.protobuf"
+        ),
+        False,
+        protogen.default_py_import_func,
+    )
 
-        # Test self references.
-        got = registry.resolve_message("acme.cloud.library.v1.Hello", "Hello")
-        self.assertIsNotNone(got)
-        self.assertEqual(got.full_name, "acme.cloud.library.v1.Hello")
+    google_protobuf_empty_message = protogen.Message(
+        google.protobuf.descriptor_pb2.DescriptorProto(name="Empty"),
+        google_protobuf_empty_file,
+        None,
+        None,
+    )
 
-        # Test self references.
-        got = registry.resolve_message("acme.cloud.library.Something", "Hello")
-        self.assertIsNotNone(got)
-        self.assertEqual(got.full_name, "acme.Hello")
+    registry._register_message(acme_hello_message)
+    registry._register_message(acme_hello_world_message)
+    registry._register_message(acme_cloud_library_v1_hello_message)
+    registry._register_message(acme_cloud_library_v1_hello_world_message)
+    registry._register_message(google_protobuf_empty_message)
+
+    got = registry.resolve_message_type("acme.cloud.library.v1.Hello", "World")
+    assert got is not None
+    assert got.full_name == "acme.cloud.library.v1.Hello.World"
+
+    got = registry.resolve_message_type("acme.Hello", "World")
+    assert got is not None
+    assert got.full_name == "acme.Hello.World"
+
+    got = registry.resolve_message_type("acme.Hello", "World")
+    assert got is not None
+    assert got.full_name == "acme.Hello.World"
+
+    got = registry.resolve_message_type("acme.cloud.library.v1.Hello", "Hello")
+    assert got is not None
+    assert got.full_name == "acme.cloud.library.v1.Hello"
+
+    got = registry.resolve_message_type("acme.cloud.library.Something", "Hello")
+    assert got is not None
+    assert got.full_name == "acme.Hello"
+
+    got = registry.resolve_message_type(
+        "acme.cloud.library.v1.Hello", "google.protobuf.Empty"
+    )
+    assert got is not None
+    assert got.full_name == "google.protobuf.Empty"
